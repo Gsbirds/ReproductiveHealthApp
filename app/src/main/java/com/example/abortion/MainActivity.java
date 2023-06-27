b013f0210a2b1695903f18545da9c7e1ec41890f
gux <gux@debian-BULLSEYE-live-builder-AMD64>
2023-06-26 18:07:12 -0600

    just api praactice with android studio #dogges

--- app/src/main/java/com/example/abortion/MainActivity.java
+++ app/src/main/java/com/example/abortion/MainActivity.java
@@ -1,6 +1,20 @@
 package com.example.abortion;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -27,11 +41,23 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-
+    ImageView mDogImageView;
+    Button nextDogButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDogImageView = findViewById(R.id.dogImageView);
+        nextDogButton = findViewById(R.id.nextDogButton);
+
+        // attaching on click listener to the button so that `loadDogImage()`
+        // function is called everytime after clicking it.
+        nextDogButton.setOnClickListener(View -> loadDogImage());
+
+        // image of a dog will be loaded once at the start of the app
+        loadDogImage();
+
         Spinner spinnerLanguages=findViewById(R.id.spinner_languages);
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
 
@@ -39,6 +65,56 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinnerLanguages.setAdapter(adapter);
         spinnerLanguages.setOnItemSelectedListener(this);
 
+    }
+
+    private void loadDogImage() {
+
+        // getting a new volley request queue for making new requests
+        RequestQueue volleyQueue = Volley.newRequestQueue(MainActivity.this);
+        // url of the api through which we get random dog images
+        String url = "https://dog.ceo/api/breeds/image/random";
+
+        // since the response we get from the api is in JSON, we
+        // need to use `JsonObjectRequest` for parsing the
+        // request response
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                // we are using GET HTTP request method
+                Request.Method.GET,
+                // url we want to send the HTTP request to
+                url,
+                // this parameter is used to send a JSON object to the
+                // server, since this is not required in our case,
+                // we are keeping it `null`
+                null,
+
+                // lambda function for handling the case
+                // when the HTTP request succeeds
+                (Response.Listener<JSONObject>) response -> {
+                    // get the image url from the JSON object
+                    String dogImageUrl;
+                    try {
+                        dogImageUrl = response.getString("message");
+                        // load the image into the ImageView using Glide.
+                        Glide.with(MainActivity.this).load(dogImageUrl).into(mDogImageView);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+
+                // lambda function for handling the case
+                // when the HTTP request fails
+                (Response.ErrorListener) error -> {
+                    // make a Toast telling the user
+                    // that something went wrong
+                    Toast.makeText(MainActivity.this, "Some error occurred! Cannot fetch dog image", Toast.LENGTH_LONG).show();
+                    // log the error message in the error stream
+                    Log.e("MainActivity", "loadDogImage error: ${error.localizedMessage}");
+                }
+        );
+
+        // add the json request object created above
+        // to the Volley request queue
+        volleyQueue.add(jsonObjectRequest);
     }
 
     @Override
